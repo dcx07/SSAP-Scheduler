ed23eec64dd32d189ef4dddefe9e0b060b90a624
Sean Deng <dcx07@hotmail.com>
2025-05-29 20:19:13 +0800

    edit main

--- Backend/main.py
+++ Backend/main.py
@@ -6,14 +6,9 @@ import os
     
 
 def main():
-    # 从 config.json 读取用户名和密码
-    with open("config.json", "r", encoding="utf-8") as f:
-        config = json.load(f)
-    username = config["username"]
-    password = config["password"]
-
+   
     # 运行 GetCK.py 抓取 cookies
-    subprocess.run(["python", "getCK_AUTO.py", username, password], check=True)
+    subprocess.run(["python", "getCK_AUTO.py"], check=True)
 
     # 运行 getSCHL.py 抓取数据
     subprocess.run(["python", "getSCHL.py"], check=True)
