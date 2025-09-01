99c0a00746e55fcd4228ae1847022bfa57557cd6
Sean Deng <dcx07@hotmail.com>
2025-09-01 21:32:26 +0800

    WIP

--- Dnet_MAUI/my-maui-app/MauiProgram.cs
+++ Dnet_MAUI/my-maui-app/MauiProgram.cs
@@ -1,3 +1,28 @@
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            佛祖保佑       永无BUG
+*/
+
+
+
 using Microsoft.Extensions.Logging;
 using MyMauiApp.ViewModels;
 using MyMauiApp.Views;
@@ -16,8 +41,6 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-
-		builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
 		builder.Services.AddLogging(configure => configure.AddDebug());
